f4ba5d02ba0443ba384ff9149d7d814b30fff67b
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-10-03 16:07:08 +0300

    creating models

--- djangoproject/djangoapp/models.py
+++ djangoproject/djangoapp/models.py
@@ -1,3 +1,14 @@
+from ipaddress import ip_address
 from django.db import models
 
 # Create your models here.
+class Computer(models.Model):
+    computer_name = models.CharField(max_length = 50)
+    ip_address = models.CharField(max_length = 50)
+    MAC_address = models.CharField(max_length = 50)
+    username = models.CharField(max_length = 50)
+    location = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.ip_address + " " + self.computer_name
+
